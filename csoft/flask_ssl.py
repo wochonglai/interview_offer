d15f0238ac756095b0d629a1556c46f306d67605
wochonglai <china.xiaowei@163.com>
2021-04-23 00:11:32 +0800

    websockets添加header

--- csoft/flask_ssl.py
+++ csoft/flask_ssl.py
@@ -15,5 +15,7 @@ def index():
 if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(__file__))
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain(path + "cert.pem", path + "key.pem", base64.b64decode("密码"))
+    context.load_cert_chain(path + "cert.crt", path + "key.key", "mypassword")
+    # 私钥加密：openssl rsa -in ssl_key_.key -aes256 -passout pass:mypassword -out ssl_encryted.key
+    # context.load_cert_chain(path + "cert.pem", path + "key.pem", base64.b64decode("密码"))
     app.run(host="0.0.0.0", port=9999, debug=True, ssl_context=context)
---
+++ csoft/websocket_server_process_request.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# WS server example
+
+import asyncio
+import websockets
+
+# async def process_request(self, path, request_headers):
+#     access_token = request_headers["access-token"]
+
+async def hello(websocket, path):
+    print(websocket.request_headers)
+    access_token = websocket.request_headers["access-token"]
+    print(access_token)
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()

d43ef79b28541d603b35271838ad051763f664ba
wochonglai <china.xiaowei@163.com>
2021-06-09 00:56:21 +0800

    pbkdf2

---
+++ pbkdf2_look.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   pbkdf2_look.py    
+@Contact :   china.xiaowei@163.com
+@License :   (C)Copyright 2021-2024, wochonglai
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2021/6/8 23:20   wayne      1.0         None
+'''
+# https://github.com/dlitz/python-pbkdf2 可学习参数处理 还有unittest
+# passlib
+# Java 参考 https://blog.csdn.net/qq_25112523/article/details/84308134
+# python加解密 https://www.php.cn/python-tutorials-377249.html
